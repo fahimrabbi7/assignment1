979504cac578a2d092fefbe34c755b7988be8cca
fahimrabbi7 <fahimmuntasirrabbi@gmail.com>
2024-09-16 22:32:24 -0500

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -5,6 +5,11 @@ number_2 = int(input("Enter number 2:"))
 number_3 = int(input("Enter number 3:"))
 number_4 = int(input("Enter number 4:"))
 
+def is_median(number_1, number_2, number_3, number_4):
+    median_value = statistics.median([number_1, number_2, number_3, number_4]) 
+    print(f"Median is {median_value}.")
+is_median(number_1, number_2, number_3, number_4)
+
 def is_prime(input_number):
     i = 2
     if input_number <= 1:
@@ -22,16 +27,11 @@ def is_prime(input_number):
             print("prime")
         i += 1
         
-
 is_prime(number_1)
 is_prime(number_2)
 is_prime(number_3)
 is_prime(number_4)
 
-def is_median(number_1, number_2, number_3, number_4):
-    median_value = statistics.median([number_1, number_2, number_3, number_4]) 
-    print(f"Median is {median_value}.")
 
-is_median(number_1, number_2, number_3, number_4)
 
 
